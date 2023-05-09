63f589648a735f260f53980f364903a9196b9f41
Jools <1035558517@qq.com>
2023-05-09 18:41:59 +0800

    Build backend initial project structure

---
+++ src/main/java/com/hspedu/Main.java
@@ -0,0 +1,12 @@
+package com.hspedu;
+
+/**
+ * @author Zexi He.
+ * @date 2023/5/9 18:29
+ * @description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/hspedu/furn/Application.java
@@ -0,0 +1,17 @@
+package com.hspedu.furn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Zexi He.
+ * @date 2023/5/9 18:35
+ * @description:
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
