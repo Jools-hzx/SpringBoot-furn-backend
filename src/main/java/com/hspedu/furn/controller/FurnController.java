d54c01549e5aa84925e4530fff6809484dcc9978
Jools <1035558517@qq.com>
2023-05-10 15:49:38 +0800

    Add function: update Furn

--- src/main/java/com/hspedu/furn/controller/FurnController.java
+++ src/main/java/com/hspedu/furn/controller/FurnController.java
@@ -1,5 +1,6 @@
 package com.hspedu.furn.controller;
 
+import com.baomidou.mybatisplus.extension.toolkit.SqlHelper;
 import com.hspedu.furn.bean.Furn;
 import com.hspedu.furn.service.FurnService;
 import com.hspedu.furn.util.Result;
@@ -24,15 +25,59 @@ public class FurnController {
 
     @PostMapping("/save")
     public Result save(@RequestBody Furn furn) {
-        furnService.save(furn);
-        return Result.success();
+        try {
+            furnService.save(furn);
+            return Result.success();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return Result.error("?", "添加失败");
+        }
     }
 
     //显示数据库中的所有数据;后面再考虑去使用分页
     @RequestMapping("/list")
     @ResponseBody
-    public Result<List<Furn>> listFurns() {
-        List<Furn> list = furnService.list();
-        return Result.success(list);
+    public Result listFurns() {
+        try {
+            List<Furn> list = furnService.list();
+            return Result.success(list);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return Result.error("?", "请求失败");
+        }
+    }
+
+    //完成数据修改
+    @PutMapping("/update")
+    @ResponseBody
+    public Result updateFurn(@RequestBody Furn furn) {
+        /*
+         * 根据 ID 选择修改
+         *
+         * @param entity 实体对象
+         *
+            default boolean updateById(T entity) {
+                return SqlHelper.retBool(getBaseMapper().updateById(entity));
+            }
+         */
+        try {
+            furnService.updateById(furn);
+            return Result.success();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return Result.error("?", "更新失败");
+        }
+    }
+
+    //根据id查询DB中的单行数据
+    @GetMapping("/query")
+    @ResponseBody
+    public Result queryById(@RequestParam(value = "id") Integer id) {
+        Furn furn = furnService.getById(id);
+        if (null != furn) {
+            return Result.success(furn);
+        } else {
+            return Result.error("4xx", "ID不存在");
+        }
     }
 }
